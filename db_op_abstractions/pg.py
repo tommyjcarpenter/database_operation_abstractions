f0443e87416d3cf3d1cd999762ee5f225d4b4b85
Tommy Carpenter <tommy@research.att.com>
2017-09-20 21:58:28 -0400

    Edit insert_multi to re-raise

--- db_op_abstractions/pg.py
+++ db_op_abstractions/pg.py
@@ -52,10 +52,10 @@ class PGConnCM(object):
 
         self.connection = psycopg2.connect(database=database, user=user, password=password, port=int(port), host=host)
         self.cursor = self.connection.cursor()
-                                                                                       
+
     def select_generator(self,stmt):
         """execute a select query and return results as a generator"""
-        data = []    
+        data = []
         try:
             self.cursor.execute(stmt)
             #change if RAM not plentiful..currently defeats the purpose of a generator
@@ -64,7 +64,7 @@ class PGConnCM(object):
                 yield row
         except Exception as msg:
             logger.error("psycopg2 SELECT ERROR!: {0}".format(msg))
-    
+
     def exec_query_list(self, Q):
         """
         execute a bunch of sql queries, commiting after each one. An error on query i does not halt query i+1
@@ -73,7 +73,7 @@ class PGConnCM(object):
         """
         succ_exec = 0
         for q in Q:
-            try: 
+            try:
                 self.cursor.execute(q)
                 self.connection.commit()
                 succ_exec+=1
@@ -84,10 +84,10 @@ class PGConnCM(object):
                 logger.error("Unknown error on: {0}, error: {1}".format(q,msg))
                 self.connection.rollback()
         logger.info("Sucessfuly executed {0} out of {1} operations".format(succ_exec, len(Q)))
-    
+
     def exec_query_list_rollback_on_error(self, Q):
         for q in Q:
-            try: 
+            try:
                 self.cursor.execute(q)
             except psycopg2.ProgrammingError as msg:
                 logger.error("Failed pg execution: {0}, error: {1}".format(q,msg))
@@ -99,29 +99,29 @@ class PGConnCM(object):
                 raise msg
         self.connection.commit() #commit only if no errors
         logger.info("Sucessfuly executed all operations")
-    
+
     def bulk_insert_chunks(self, table_name, values, chunk_size=1000):
         """
         Bulk insert via batches., commiting after each batch. If any of the batches fails, insertions continue by
         attempting the next batch. Unexpected failures are only logged.
 
-        Returns the total number of records sucessfully inserted. 
+        Returns the total number of records sucessfully inserted.
         """
         logger.info("Insert: {0}records, {1}records/chunk".format(len(values), chunk_size))
-    
+
         def chunks(src, chunk_size):
             chunk_count = int(len(src)/chunk_size)
             chunk_count = 1 if chunk_count == 0 else chunk_count
             for i in range(0, chunk_count):
                 yield src[i*chunk_size:(i+1)*chunk_size] \
                     if chunk_count-1 > i else src[i*chunk_size:]
-    
+
         with Timer() as ct:
             num_records = [self.bulk_insert(table_name, chunk) for chunk in chunks(values, chunk_size) ]
             logger.info("Done inserting: {0}records inserted, {1}s".format(sum(num_records), ct.elapsed))
-    
+
         return sum(num_records)
-    
+
     def bulk_insert(self, table_name, values):
         """
         Bulk insert
@@ -129,38 +129,38 @@ class PGConnCM(object):
         values is a list of tuples to insert
 
         Upon an exception, the entire batch fails and the progress is rolled back.
-        If you want to execute smaller batches where successive batches are tried 
-        regardless of previous failures, use bulk_insert_batches instead of this function directly. 
+        If you want to execute smaller batches where successive batches are tried
+        regardless of previous failures, use bulk_insert_batches instead of this function directly.
 
-        Returns the number of records inserted, or 0 if there was a logged exception. 
+        Returns the number of records inserted, or re-raises the exception after a rollback
         """
-    
+
         num_fields = len(values[0])
         params = ','.join(['%s' for i in range(0, num_fields)])
         params = "({0})".format(params)
         try:
-                    # In python3.4 version of psycopg2, mogrify returns a byte string.
-                    # while 2.7 version returns a string.
-                    # Thus, always write the values into byte string first then convert
-                    # to unicode
-                    str_values = b','.join([self.cursor.mogrify(params, x) for x in values])
-                    str_values = str_values.decode('utf-8')
-                    query = u"INSERT INTO {0} VALUES {1}".format(table_name, str_values)
-                    self.cursor.execute(query)
-                    self.connection.commit()
-                    return len(values)
-        except psycopg2.ProgrammingError as msg:
-                logger.error("Failed pg bulk_insert: error: {0}".format(msg))
-                self.connection.rollback()
-                return 0
-        except Exception as msg:
-                logger.error("Unknown error: {1}".format(msg))
-                self.connection.rollback()
-                return 0
-    
+               # In python3.4 version of psycopg2, mogrify returns a byte string.
+               # while 2.7 version returns a string.
+               # Thus, always write the values into byte string first then convert
+               # to unicode
+               str_values = b','.join([self.cursor.mogrify(params, x) for x in values])
+               str_values = str_values.decode('utf-8')
+               query = u"INSERT INTO {0} VALUES {1}".format(table_name, str_values)
+               self.cursor.execute(query)
+               self.connection.commit()
+               return len(values)
+        except psycopg2.ProgrammingError as e:
+            logger.error("Failed pg bulk_insert: error: {0}".format(e))
+            self.connection.rollback()
+            raise e
+        except Exception as e:
+            logger.error("Unknown error: {0}".format(e))
+            self.connection.rollback()
+            raise e
+
     def get_all_tables(self):
         """
-        No easy way to get the equivelent of \dt in SQL form. 
+        No easy way to get the equivelent of \dt in SQL form.
         This just does the answer from: http://stackoverflow.com/questions/14730228/postgres-query-to-list-all-table-names
         """
         stmt = """
@@ -177,7 +177,7 @@ class PGConnCM(object):
         There is no way to easily get the EXACT row count of all tables in a database:
         http://stackoverflow.com/questions/2596670/how-do-you-find-the-row-count-for-all-your-tables-in-postgres
 
-        If you can't rely on one of the estimates there, this function computes it exactly. 
+        If you can't rely on one of the estimates there, this function computes it exactly.
 
         Returns a list of tuples [(table_name, row_count)]
         """
@@ -188,5 +188,5 @@ class PGConnCM(object):
             ret.append((t, int(self.cursor.fetchone()[0])))
         return ret
 
-    
-    
+
+
